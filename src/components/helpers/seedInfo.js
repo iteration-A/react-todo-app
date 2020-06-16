cd1d4e0d101500387366dcfbff823e45f153dcf6
Manuel Luna <manuellunada@gmail.com>
2020-06-15 23:34:19 -0500

    Fix bug in seedInfo that makes todos be null

--- src/components/helpers/seedInfo.js
+++ src/components/helpers/seedInfo.js
@@ -1,27 +1,5 @@
-// const todosTest = [
-//   {
-//     name: "kill ma self",
-//     info: "just that",
-//     status: "done",
-//     category: "life",
-//   },
-//   {
-//     name: "kill ma self",
-//     info: "just that",
-//     status: "doing",
-//     category: "life",
-//   },
-//   {
-//     name: "kill ma self",
-//     info:
-//       "just that dasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasddasdasdasdasasasassssssssssssssssssssssssssssssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssssssss",
-//     status: "pending",
-//     category: "aa",
-//   },
-// ];
-
 const checkLocalStorage = () => {
-  if (window.localStorage.getItem("todos") === "null") return [];
+  if (window.localStorage.getItem("todos") === null) return [];
   return JSON.parse(window.localStorage.getItem("todos"));
 };
 
